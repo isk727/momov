b8cd94eac92529c9e5e45e9bb0c4ac84de9bb9a0
isk727 <isk727@gmail.com>
2024-03-06 05:22:25 +0900

    Update scriptv.js

--- scriptv.js
+++ scriptv.js
@@ -1,17 +1,15 @@
 const video = document.getElementById('remote_video');
 $("#sound").on('click', function () {
-  if(video.muted){
-    $("#speaker").attr('src', 'img/sp_on.png'); // KX
+  if (video.muted) {
+    $("#speaker").attr('src', 'img/sp_on.png');
     video.muted = false;
-  }else{
-    $("#speaker").attr('src', 'img/sp_off.png'); // KX
+  } else {
+    $("#speaker").attr('src', 'img/sp_off.png');
     video.muted = true;
   }
 });
-//connect();
-//video.className = "rotate90";
-window.setTimeout(function(){
+window.setTimeout(function() {
   connect();
   video.className = "rotate90";
-  $("#sound").css('visibility', 'visible'); // 20240304
+  $("#sound").css('visibility', 'visible');
 }, 1000);
